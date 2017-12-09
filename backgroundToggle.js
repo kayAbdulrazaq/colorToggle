020b4db0e3afb1ee548e059d501faf9ca2c63721
Kay Abdulrazaq <kkmekaroom@gmail.com>
2017-12-09 17:23:00 +0100

    Add comments for JS code

--- backgroundToggle.js
+++ backgroundToggle.js
@@ -1,8 +1,10 @@
+ /* The following Javascript code selects 'button' on html page and attaches an event listener to it, 
+which then toggles the classList 'green' that is created in the CSS file. */
 
-var btn = document.querySelector("button");
+var btn = document.querySelector("button"); 
 
 btn.addEventListener('click', function(){
-  document.body.classList.toggle("green");
+  document.body.classList.toggle("green"); 
 });
 
 
